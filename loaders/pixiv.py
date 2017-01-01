f84e5a200ff8fcb066e085c92cc5ce96613b1710
AmazingThew <amazingthew@gmail.com>
2016-12-31 23:29:14 -0600

    Updated Pixiv API usage; manga and R18 works load correctly now

--- loaders/pixiv.py
+++ loaders/pixiv.py
@@ -2,7 +2,8 @@ import datetime
 import os
 import pytz
 import requests
-from pixivpy3 import PixivAPI, PixivError
+from pixivpy3 import PixivAPI
+from pprint import pprint
 
 
 # Info needed:
@@ -41,69 +42,54 @@ class Pixiv(object):
     def loadWorks(self):
         print('Retrieving Pixiv works')
         self.authorize()
-        feeds = self.api.me_feeds()
-        workIds = [r['ref_work']['id'] for r in feeds['response'] if r['type'] == 'add_illust']
-        workDicts = [self._getWorkDict(workId) for workId in workIds]
-        works = [w for workDict in workDicts for w in self._getImageData(workDict)]
-        return works
+        apiWorks = self.api.me_following_works()
+        workDicts = apiWorks['response']
+        workDicts = [w for w in workDicts]
+        [self._getImageData(workDict) for workDict in workDicts]
 
 
-    def _getWorkDict(self, workId):
-        url = 'https://public-api.secure.pixiv.net/v1/works/' + workId + '.json'
-        result = self.api.auth_requests_call('GET', url)
-        try:
-            return self.api.parse_result(result)
-        except PixivError as p:
-            return {'status' : 'failure', 'errors' : str(p)}
+    def _getImageData(self, imageDict):
+        imageData = {
+            'identifier'      : '',
+            'authorName'      : '',
+            'authorHandle'    : '',
+            'authorAvatarUrl' : '',
+            'profileUrl'      : '',
+            'website'         : '',
+            'imageTitle'      : '',
+            'imageUrls'       : [],
+            'imagePageUrl'    : '',
+            'imageTimestamp'  : '',
+            'imageType'       : '',
+            'nsfw'            : False,
+            'width'           : '500',
+            'height'          : '500',
+            'success'         : False,
+            'error'           : 'Unknown error',
+        }
 
-    def _getImageData(self, workDict):
-        imageList = []
-        for imageDict in workDict['response']:
-            imageData = {
-                'identifier'      : '',
-                'authorName'      : '',
-                'authorHandle'    : '',
-                'authorAvatarUrl' : '',
-                'profileUrl'      : '',
-                'website'         : '',
-                'imageTitle'      : '',
-                'imageUrls'       : [],
-                'imagePageUrl'    : '',
-                'imageTimestamp'  : '',
-                'imageType'       : '',
-                'nsfw'            : False,
-                'width'           : '500',
-                'height'          : '500',
-                'success'         : False,
-                'error'           : 'Unknown error',
-            }
+        identifier = str(imageDict.get('id'))
+        if identifier not in self.dbDict['works']: # Skip images we've already loaded
+            user = imageDict.get('user') or {}
+            imageData['identifier']      = identifier
+            imageData['authorName']      = str(user.get('name'))
+            imageData['authorHandle']    = str(user.get('account'))
+            imageData['authorAvatarUrl'] = self._getAvatarUrl(str((user.get('profile_image_urls') or {}).get('px_50x50')))
+            imageData['profileUrl']      = 'http://www.pixiv.net/member.php?id=' + str(user.get('id'))
+            imageData['website']         = 'Pixiv'
+            imageData['imageTitle']      = str(imageDict.get('title'))
+            imageData['imageUrls']       = self._getImageUrls(imageDict)
+            imageData['imagePageUrl']    = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + str(imageDict.get('id'))
+            imageData['imageTimestamp']  = str(max(imageDict.get('created_time'), imageDict.get('reupoloaded_time', '')))
+            imageData['imageType']       = str(imageDict.get('type'))
+            imageData['nsfw']            = str(imageDict.get('age_limit') != 'all-age')
+            imageData['width']           = str(imageDict.get('width')) or '500'
+            imageData['height']          = str(imageDict.get('height')) or '500'
+            imageData['success']         = str(imageDict.get('status') == 'success')
+            imageData['error']           = str(imageDict.get('errors'))
 
-            if workDict['status'] == 'success':
-                identifier = str(imageDict.get('id'))
-                if identifier not in self.dbDict['works']: # Skip images we've already loaded
-                    user = imageDict.get('user') or {}
-                    imageData['identifier']      = identifier
-                    imageData['authorName']      = str(user.get('name'))
-                    imageData['authorHandle']    = str(user.get('account'))
-                    imageData['authorAvatarUrl'] = self._getAvatarUrl(str((user.get('profile_image_urls') or {}).get('px_50x50')))
-                    imageData['profileUrl']      = 'http://www.pixiv.net/member.php?id=' + str(user.get('id'))
-                    imageData['website']         = 'Pixiv'
-                    imageData['imageTitle']      = str(imageDict.get('title'))
-                    imageData['imageUrls']       = self._getImageUrls(imageDict)
-                    imageData['imagePageUrl']    = 'http://www.pixiv.net/member_illust.php?mode=medium&illust_id=' + str(imageDict.get('id'))
-                    imageData['imageTimestamp']  = str(max(imageDict.get('created_time'), imageDict.get('reupoloaded_time', '')))
-                    imageData['imageType']       = str(imageDict.get('type'))
-                    imageData['nsfw']            = str(imageDict.get('age_limit') != 'all-age')
-                    imageData['width']           = str(imageDict.get('width')) or '500'
-                    imageData['height']          = str(imageDict.get('height')) or '500'
-                    imageData['success']         = str(imageDict.get('status') == 'success')
-                    imageData['error']           = str(imageDict.get('errors'))
+            self.dbDict['works'][identifier] = imageData
 
-                    self.dbDict['works'][identifier] = imageData
-            else:
-                raise RuntimeError('Failed Pixiv API call: ' + workDict.get('errors'))
-
-        return imageList
 
     def _parseTime(self, imageDict):
         s = max(imageDict.get('created_time', ''), imageDict.get('reupoloaded_time', ''))
@@ -114,16 +100,22 @@ class Pixiv(object):
 
     def _getImageUrls(self, imageDict):
         workType = imageDict.get('type')
-        if workType == 'illustration':
-            urlDict = imageDict.get('image_urls') or {}
-            urls = [self._generateImageUrl(urlDict.get('small') or urlDict.get('medium') or urlDict.get('large'))]
 
-        elif workType == 'manga':
-            pages = (imageDict.get('metadata') or {}).get('pages') or []
+        if imageDict.get('is_manga') == True or workType == 'manga':
+            response = self.api.works(imageDict['id'])
+            response = response.get('response')[0] or {}
+            metadata = response.get('metadata') or {}
+            pages = metadata.get('pages') or []
+
             def getMangaUrl(d):
                 urld = d.get('image_urls')
                 return self._generateImageUrl(urld.get('small') or urld.get('medium') or urld.get('large'))
+
             urls = [getMangaUrl(item) for item in pages]
+
+        elif workType == 'illustration':
+            urlDict = imageDict.get('image_urls') or {}
+            urls = [self._generateImageUrl(urlDict.get('small') or urlDict.get('medium') or urlDict.get('large'))]
 
         # Ugoira handling falls through to default for now
         # elif workType == 'ugoira':
